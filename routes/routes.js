03cbde92009852b7601ee876800c89aff1c6c13f
NadhirotulLaily <nadhirotullaily1207@gmail.com>
2023-11-22 22:21:02 +0700

    updated

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,6 @@
-// routes.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const controllers = require('./controllers');
+const controllers = require('../controllers');
 
 const router = express.Router();
 router.use(bodyParser.json());
