3080ad2d36baf3930a4e712a86916cedcbc4a03c
meisetiawati <meisetiawaty@gmail.com>
2023-11-24 23:10:52 +0800

    form input account member

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const morgan = require('morgan');
 const swaggerJsDoc = require('swagger-jsdoc');
@@ -10,44 +9,14 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-=======
-require('dotenv').config()
-
-const express        = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const ejs            = require('ejs')
-const route          = require('./routes/index')
-const errorHandler   = require('./middlewares/error-handler')
-const cors           = require('cors')
-const bodyParser     = require('body-parser')
-const path           = require('path')
-var app              = express()
-const port           = process.env.PORT || 3000
-const morgan         = require('morgan');
-const swaggerJsDoc   = require('swagger-jsdoc');
-const swaggerUI      = require('swagger-ui-express');
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.set('views', path.join(__dirname,'public/views/'))
-app.set('view engine', 'ejs')
-app.use(expressLayouts)
->>>>>>> 610c98bea290a8e57354ac19b8d2cfe51f2a786d
 
 const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.1.9',
     info: {
-<<<<<<< HEAD
       title: 'API E-commerce',
       version: '1.0.0',
       description: 'Informasi API E-commerce',
-=======
-      title: 'API Admin',
-      version: '1.0.0',
-      description: 'Informasi API Admin',
->>>>>>> 610c98bea290a8e57354ac19b8d2cfe51f2a786d
       servers: ['http://localhost:3000'],
     },
     components: {
@@ -62,7 +31,6 @@ const swaggerOptions = {
   },
   apis: ['./routes/user.js'],
 };
-<<<<<<< HEAD
 
 // Middleware untuk memproses payload JSON
 app.use(express.json());
@@ -80,18 +48,3 @@ app.use(errorHandling);
 app.listen(PORT, () => {
   console.log(`Server running pada port ${PORT}`);
 });
-=======
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-app.use(morgan('common'));
-
-app.use('/', route)
-
-// app.use(errorHandler);
-
-if (process.env.APP_ENV != 'test') {
-    app.listen(port, () => {
-        console.log(`Listening on http://localhost:${port}`)
-    })
-}
->>>>>>> 610c98bea290a8e57354ac19b8d2cfe51f2a786d
--- routes/user.js
+++ routes/user.js
@@ -1,69 +1,25 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const { registerMember, getAllMembers } = require('../controllers/user');
-=======
-const { registerAdmin, login, getAllAdmins, getAdminById} = require('../controllers/user');
->>>>>>> 610c98bea290a8e57354ac19b8d2cfe51f2a786d
 const { authentication } = require('../middlewares/auth');
 
 /**
  * @swagger
  * tags:
-<<<<<<< HEAD
  *   name: Members
  *   description: Manajemen Members
-=======
- *   name: Users
- *   description: Manajemen Users
->>>>>>> 610c98bea290a8e57354ac19b8d2cfe51f2a786d
  */
 
 /**
  * @swagger
-<<<<<<< HEAD
  * /users/register:
  *   post:
  *     summary: Registrasi member baru
  *     tags: [Members]
-=======
- * components:
- *   schemas:
- *     User:
- *       type: object
- *       properties:
- *         email:
- *           type: string
- *         password:
- *           type: string
- *         levelUser:
- *           type: string
- *     UserLogin:
- *       type: object
- *       properties:
- *         email:
- *           type: string
- *         password:
- *           type: string
- *   securitySchemes:
- *     MyAuth:
- *       type: http
- *       scheme: bearer
- *       bearerFormat: JWT
- */
-
-/**
- * @swagger
- * /user/admins/register:
- *   post:
- *     summary: Registrasi admin baru
- *     tags: [Users]
->>>>>>> 610c98bea290a8e57354ac19b8d2cfe51f2a786d
  *     requestBody:
  *       content:
  *         application/json:
  *           schema:
-<<<<<<< HEAD
  *             type: object
  *             properties:
  *               userId:
@@ -103,91 +59,5 @@ router.post('/register', registerMember);
  *         description: Internal Server Error!
  */
 router.get('/', authentication, getAllMembers);
-=======
- *             $ref: '#/components/schemas/User'
- *     responses:
- *       201:
- *         description: Registrasi Admin berhasil!
- *       400:
- *         description: Invalid data or email already registered
- *       500:
- *         description: Internal Server Error
- */
-router.post('/admins/register', registerAdmin);
-
-/**
- * @swagger
- * /user/login:
- *   post:
- *     summary: Login user (admin or member)
- *     tags: [Users]
- *     requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/UserLogin'
- *     responses:
- *       200:
- *         description: Login berhasil!
- *       401:
- *         description: Email atau password salah!
- *       500:
- *         description: Internal Server Error!
- */
-router.post('/login', login);
-
-/**
- * @swagger
- * /user/admins:
- *   get:
- *     summary: Get all admins
- *     tags: [Users]
- *     security:
- *       - MyAuth: []
- *     responses:
- *       200:
- *         description: Admins berhasil diambil!
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *       401:
- *         description: Unauthorized
- *       500:
- *         description: Internal Server Error!
- */
-router.get('/admins', authentication, getAllAdmins);
-
-/**
- * @swagger
- * /user/admins/{id}:
- *   get:
- *     summary: Get admin by ID
- *     tags: [Users]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: Admin ID
- *         schema:
- *           type: integer
- *     security:
- *       - MyAuth: []
- *     responses:
- *       200:
- *         description: Admin berhasil diambil!
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/User"
- *       401:
- *         description: Unauthorized
- *       404:
- *         description: Admin not found
- *       500:
- *         description: Internal Server Error!
- */
-router.get('/admins/:id', authentication, getAdminById);
->>>>>>> 610c98bea290a8e57354ac19b8d2cfe51f2a786d
 
 module.exports = router;
