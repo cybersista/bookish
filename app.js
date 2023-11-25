32ad47250a644237aa49fd9b026aedafdc52e315
NadhirotulLaily <nadhirotullaily1207@gmail.com>
2023-11-25 11:59:48 +0700

    updated

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
 
-const express = require('express')
+const express        = require('express')
 const expressLayouts = require('express-ejs-layouts')
-<<<<<<< HEAD
 const ejs            = require('ejs')
 const route          = require('./routes/index')
 const errorHandler   = require('./middlewares/error-handler')
@@ -20,23 +19,11 @@ const cartRoutes = require('./routes/cartRoutes');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-=======
-const ejs = require('ejs')
-const route = require('./routes/index')
-const errorHandler = require('./middlewares/error-handler')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const path = require('path')
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./config/swagger.json');
-const appRoutes = require('./routes');
 
-const app = express();
-const port = 3000;
->>>>>>> 03cbde92009852b7601ee876800c89aff1c6c13f
+app.set('views', path.join(__dirname,'public/views/'))
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
 
-
-<<<<<<< HEAD
 const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.0.0',
@@ -59,7 +46,7 @@ app.use('/', route)
 
 app.use('/api', cartRoutes); 
 
-// app.use(errorHandler);
+
 
 
 if (process.env.APP_ENV != 'test') {
@@ -67,22 +54,3 @@ if (process.env.APP_ENV != 'test') {
         console.log(`Listening on http://localhost:${port}`)
     })
 }
-=======
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', appRoutes);
-
-app.use('/api-docs', swaggerUi.serve);
-app.get('/api-docs', swaggerUi.setup(swaggerDocument));
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
->>>>>>> 03cbde92009852b7601ee876800c89aff1c6c13f
