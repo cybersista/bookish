4217de39febc1952fc91f6c8fa8f115da9fdd018
Shella Febriana <shellafebriana14@gmail.com>
2023-12-02 20:08:30 +0700

    dashboard user

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,16 +1,32 @@
-const {ulasanBuku, Buku, Kategori, sequelize } = require('../models')
+const {ulasanBuku, Buku, Kategori, fileBuku, sequelize } = require('../models')
 const dashboardUser = async(req, res, next) => {
     try {
-        const all = await Buku.findAll();
+        const all = await Buku.findAll({
+            attributes: ['id', 'judul', 'harga'],
+            include : {
+                model : fileBuku,
+                as : 'fileBukus',
+                attributes : ['urlFile']
+            },
+            group : ['Buku.id'],
+            limit : 10
+        });
         const popularBooks = await Buku.findAll({
             attributes: ['id', 'judul', 'harga', [sequelize.fn('AVG', sequelize.col('ulasanBukus.rating')), 'averageRating']],
-            include: [{ 
-                model: ulasanBuku, 
-                as : 'ulasanBukus',
-                attributes: []
-            }],
-            group : ['Buku.id'],
-            order : [[sequelize.col('averageRating'), 'DESC']],
+            include:[
+                {
+                    model : ulasanBuku,
+                    as : 'ulasanBukus',
+                    attributes: [],
+                },
+                {
+                    model : fileBuku,
+                    as : 'fileBukus',
+                    attributes : ['urlFile']
+                }
+            ],
+            group : ['Buku.id', 'ulasanBukus.id', 'fileBukus.id'],
+            order : [[sequelize.col('averageRating'), 'DESC']]
         })
         const recommendedbyCategoryBooks = await Kategori.findAll({
             include : {
@@ -24,7 +40,7 @@ const dashboardUser = async(req, res, next) => {
               
             }
         })
-        res.status(200).json({ status: 200, message: 'Success', data: {recommendedbyCategoryBooks,all,popularBooks }});
+        res.status(200).json({ status: 200, message: 'Success', data: {all, popularBooks,recommendedbyCategoryBooks }});
     } catch (err) {
         console.log(err)
     }
