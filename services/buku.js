1482ab5d97b645ffd3640a70be53e91d89c7aa47
Shella Febriana <shellafebriana14@gmail.com>
2023-12-02 20:29:01 +0700

    delete repositories and services

--- services/buku.js
+++
@@ -1,9 +0,0 @@
-const  BukuRepository  = require('../repositories/buku')
-
-function simpleGet(id) {
-    const attributes = ['id','judul','harga','isbn','tahunTerbit']
-    return BukuRepository.attributesFind(id, attributes)
-}
-module.exports = {
-    simpleGet
-}
